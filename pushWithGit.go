47b186799b3c83e98b755f7b86770f63c1787ace
Joseph Marrero <jmarrero@redhat.com>
2019-06-20 13:49:59 -0400

    random dir name and cleanup

--- pushWithGit.go
+++ pushWithGit.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
-    "encoding/json"
-    "os/exec"
+	"os/exec"
+	"time"
 
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
@@ -20,8 +22,8 @@ var (
 )
 
 type repository_and_branch struct {
-    Repo string `json:"repo"`
-    Branch string `json:"branch"`
+	Repo   string `json:"repo"`
+	Branch string `json:"branch"`
 }
 
 // curl -d '{"repo":"https://github.com/jmarrero/test-adocs.git", "branch":"master"}' -H "Content-Type: application/json" -X POST http://localhost:9666/clone
@@ -31,47 +33,76 @@ func cloneBranch(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			http.Error(w, "Error reading request body",
 				http.StatusInternalServerError)
-        }
+		}
 
-        var repo repository_and_branch
-        err = json.Unmarshal(body, &repo)
+		var repo repository_and_branch
+		err = json.Unmarshal(body, &repo)
 
-        log.Println(repo.Repo)
-        log.Println(repo.Branch)
+		log.Println(repo.Repo)
+		log.Println(repo.Branch)
 
-        repository := repo.Repo
+		repository := repo.Repo
 		branch := repo.Branch
-		directory := "/home/jmarrero/g/repo"
+		directory := "/home/jmarrero/g/" + randomAlphaNumericString()
 
-		log.Print("git clone -b %s --single-branch %s %s", branch, repository, directory)
-
-		_, err = git.PlainClone(directory, false, &git.CloneOptions{
-			URL:           repository,
-			ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branch)),
-			SingleBranch:  true,
-			Progress:      os.Stdout,
-        })
-
-        if err == nil {
-        //Now call python
-        args := []string{"push", "--directory", "/home/jmarrero/g/repo"}
-        cmd := exec.Command("/home/jmarrero/g/pantheon.py", args...)
-        out, err := cmd.Output()
-
-        log.Print(err)
-        log.Print(string(out))
-        //Now cleanup
-        //remove all cloned things//todo
-        } else {
-            log.Print(err)
-        }
-        
-
+		//start a new goroutine (lightweight thread) to handle clone/push/cleanup
+		go gitClone(repository, branch, directory)
 
 		fmt.Fprint(w, "POST done")
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
+}
+
+func gitClone(repository string, branch string, directory string) {
+	log.Printf("git clone -b %s --single-branch %s %s", branch, repository, directory)
+
+	_, err := git.PlainClone(directory, false, &git.CloneOptions{
+		URL:           repository,
+		ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branch)),
+		SingleBranch:  true,
+		Progress:      os.Stdout,
+	})
+
+	if err == nil {
+		//Uploader call still on the same gorutine.
+		push2pantheon(directory)
+	} else {
+		log.Print(err)
+	}
+
+}
+
+func push2pantheon(directory string) {
+	//Now call python
+	args := []string{"push", "--directory", directory}
+	cmd := exec.Command("/home/jmarrero/g/pantheon.py", args...) //TODO
+	out, err := cmd.Output()
+
+	log.Print(err)
+	log.Print(string(out))
+
+	// Cleanup also happens in the same thread.
+	cleanup(directory)
+}
+
+func cleanup(directory string) {
+	//Todo
+	log.Printf("Cleanup: deleting directory: %s", directory)
+	os.RemoveAll(directory)
+}
+
+func randomAlphaNumericString() string {
+	const chars = "0123456789abcdefghijklmnopqrstuvwxyzREDHAT"
+
+	var seed *rand.Rand = rand.New(
+		rand.NewSource(time.Now().UnixNano()))
+
+	byteSlice := make([]byte, 10)
+	for i := range byteSlice {
+		byteSlice[i] = chars[seed.Intn(len(chars))]
+	}
+	return string(byteSlice)
 }
 
 func init() {
